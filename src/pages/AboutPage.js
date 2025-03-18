7eaad79b1be6d3b54a573b5f79f764994673feed
vvh24 <valeriahcrespo@hotmail.com>
2025-03-18 17:11:57 -0400

    aboutpages.js

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import About from '../components/sections/About';
+
+function AboutPage() {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+}
+
+export default AboutPage;
